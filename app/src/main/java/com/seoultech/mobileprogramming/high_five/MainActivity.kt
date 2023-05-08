163b52b82309815fd5df3b897a5754904f456298
eunbin00 <eun61n@gmail.com>
2023-05-08 11:42:37 +0900

    [FIX] rename package name

--- app/src/main/java/com/seoultech/mobileprogramming/high_five/MainActivity.kt
+++ app/src/main/java/com/seoultech/mobileprogramming/high_five/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.high_five
+package com.seoultech.mobileprogramming.high_five
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.high_five.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
